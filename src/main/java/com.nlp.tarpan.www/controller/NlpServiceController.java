eb5d4fd281f459457c40b9906ddb5d402db9f929
yinzhang.lu <sekift@163.com>
2021-02-05 18:07:55 +0800

    优化
    优化

--- src/main/java/com.nlp.tarpan.www/controller/NlpServiceController.java
+++ src/main/java/com.nlp.tarpan.www/controller/NlpServiceController.java
@@ -2,7 +2,7 @@ package com.nlp.tarpan.www.controller;
 
 import com.nlp.tarpan.www.service.NlpService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class NlpServiceController {
     @Autowired
     private NlpService nlpService;
 
-    @GetMapping("/parser")
+    @PostMapping("/parser")
     public Map<String, String> parser(String input){
         return nlpService.parser(input);
     }
